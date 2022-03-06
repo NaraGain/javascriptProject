0b54d5591408ed9b93e8b0fd957c0edf5cc86952
Bun Nara <bunnarachoch@gmail.com>
2022-03-06 22:31:11 +0700

    add new file

--- weather/index.js
+++ weather/index.js
@@ -1,20 +1,55 @@
-let  weather = {
-    api_key : ' e327a62c5a1554a74d15150619ce63e5',
-    fetchWeather: function (city) {
-        fetch(
-           " https://api.openweathermap.org/data/2.5/weather?q="+ city +
-       " &appid=" + this.api_key
-          )
-          .then((response) => {
-            if (!response.ok) {
-              alert("No weather found.");
-             
-            }
-            return response.json();
-          })
-          .then((data) => console.log(data));
-      },
+function weather(city){
+  try{
+      fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=e327a62c5a1554a74d15150619ce63e5")
+      .then(Response => Response.json())
+      .then( data =(data)=>{
+ displayweather(data)
+
+ 
+      } )
+  }catch(err){
+      console.log(err)
+    
+  }
 }
 
 
-weather.fetchWeather('new york')
+
+
+
+function displayweather(data){
+const {country} = data.sys;
+const {name} = data
+const {temp,humidity} = data.main;
+const {speed} = data.wind
+const { icon, description } = data.weather[0];
+
+document.querySelector('.city').innerHTML = "Weather in : "  + name
+document.querySelector('.country').innerHTML = "Country Code :"  + country
+var img = document.querySelector('.img')
+img.src =
+"https://openweathermap.org/img/wn/" + icon+ ".png";
+img.style.backgroundColor = '#8b8f8e'
+document.querySelector('.temp').innerHTML = `Tempeture : ${temp} °C`
+document.querySelector('.humidity').innerHTML = humidity + "% humidity  "
+document.querySelector('.wind').innerHTML = "wind speed :"  + speed + "k/m.s"
+
+
+document.querySelector('.desc').innerHTML = ""  + description + ""
+
+}
+
+
+
+
+var btnsearch = document.getElementById('btnSearch');
+
+btnsearch.addEventListener('click',function(){
+var text = document.getElementById('text').value
+var showContainer = document.getElementById('show')
+showContainer.style.backgroundColor = '#F2F2F2'
+showContainer.style.color = "#535555"
+
+
+weather(text)
+})
